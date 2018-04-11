b4551b341074d20fb745e387312562dd712867c4
Dave Bull <dave@davebull.net>
2018-04-11 05:52:11 +0100

    Initial commit

---
+++ ADSBSharp/AircraftSummary.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualRadar.Interface.Adsb;
+
+namespace ADSBSharp
+{
+    class AircraftSummary
+    {
+        public string IcaoCode { get; set; }
+
+        public string Registration { get; set; }
+
+        public byte? SIL { get; set; }
+
+        public SystemDesignAssurance? SDA { get; set; }
+
+        public double? Airspeed { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("ICAO: {0}, Registration: {1}, SIL: {2}, SDA: {3}, Airspeed: {4}", IcaoCode, Registration, SIL, SDA, Airspeed);
+        }
+    }
+}
